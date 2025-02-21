c86250d9d09e18853fa99498b3eacc0cf8e8f464
K-Jonathan <Jonathan.koenning@gmail.com>
2025-02-22 00:24:55 +0100

    Header

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,20 @@
 </head>
 
 <body>
+    <header class="navbar">
+        <div class="container">
+            <a href="index.php" class="logo img">
+                <img src="images/flamin-go-logo.png" alt="Flamin-Go Car Rental Logo">
+            </a>
+            <nav>
+                <ul>
+                    <li><a href="#" class="icon-text"><img src="images/car-icon.png" alt="My bookings icon"><span>Meine Buchungen</span></a></li>
+                    <li><a href="#" class="icon-text"><img src="images/world-icon.png" alt="World icon"><span>DE</span></a></li>
+                    <li><a href="#" class="icon-text"><img src="images/person-icon.png" alt="Person icon"><span>Anmelden | Login</span></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 <?php
 echo "Header";
 ?>
