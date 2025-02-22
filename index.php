a7c2d32e4986bb90b23f9e070d3c139ed9633b5f
K-Jonathan <Jonathan.koenning@gmail.com>
2025-02-22 03:56:29 +0100

    Key Features Section

--- index.php
+++ index.php
@@ -35,3 +35,27 @@ include("includes/header.php");
     <h1>MIETE PINK - FAHR GRÜN</h1>
     <h2>Nachhaltige Autovermietung einfach gemacht</h2>
  </section>
+
+ <section class="features">
+    <div class="feature">
+        <div class="feature-header">
+            <img src="images/maps-icon.png" alt="Zuverlässig">
+            <h3>Zuverlässig</h3>
+        </div>
+        <p><strong>Über 40 Städte und mehr als 400 Fahrzeuge für flexible Mobilität</strong></p>
+    </div>
+    <div class="feature">
+        <div class="feature-header">
+            <img src="images/couple-icon.png" alt="Aufmerksam">
+            <h3>Aufmerksam</h3>
+        </div>
+        <p><strong>Persönlicher Kundenservice mit schneller und zuverlässiger Betreuung</strong></p>
+    </div>
+    <div class="feature">
+        <div class="feature-header">
+            <img src="images/tree-icon.png" alt="Naturbewusst">
+            <h3>Naturbewusst</h3>
+        </div>
+        <p><strong>Für jeden Mietwagen pflanzen wir einen Baum, seien Sie umweltfreundlich unterwegs</strong></p>
+    </div>
+</section>
