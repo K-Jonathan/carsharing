65205b563624ba4f61cd82561e931ec3544700d8
K-Jonathan <Jonathan.koenning@gmail.com>
2025-03-17 01:24:08 +0100

    header.php - Formatierung & Logingpage.php Verknüpfung

--- includes/header.php
+++ includes/header.php
@@ -14,16 +14,25 @@ include 'session_handler.php'; // 🔹 Stellt sicher, dass `userid` immer verfü
             <a href="/carsharing/" class="logo img">
                 <img src="images/flamin-go-logo.png" alt="Flamin-Go Car Rental Logo">
             </a>
-
-            <!-- 🔹 Falls eingeloggt, zeige den Username -->
-            <?php if (isset($_SESSION['userid'])): ?>
-                <span class="logged-in-user">Guten Tag, <strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong></span>
-<?php endif; ?>
-
-
+            
             <nav>
                 <ul>
-                    <li><a href="bookings.php" class="icon-text"><img src="images/car-icon.png" alt="My bookings icon"><span>Meine Buchungen</span></a></li>
+                <?php if ($logged_in): ?>
+    <li>
+        <a href="bookings.php" class="icon-text">
+            <img src="images/car-icon.png" alt="My bookings icon">
+            <span>Meine Buchungen</span>
+        </a>
+    </li>
+<?php else: ?>
+    <li>
+        <a href="loginpage.php" class="icon-text">
+            <img src="images/car-icon.png" alt="My bookings icon">
+            <span>Meine Buchungen</span>
+        </a>
+    </li>
+<?php endif; ?>
+
                     <li><a href="#" class="icon-text"><img src="images/world-icon.png" alt="World icon"><span>DE</span></a></li>
 
                     <!-- 🔹 Anmelden/Login oder Abmelden/Logout abhängig vom Status -->
@@ -31,7 +40,7 @@ include 'session_handler.php'; // 🔹 Stellt sicher, dass `userid` immer verfü
                         <li>
     <a href="logout.php" class="icon-text">
         <img src="images/person-icon.png" alt="Person icon">
-        <span>Abmelden | Logout</span>
+        <span><strong><?php echo htmlspecialchars($_SESSION['username']); ?></strong> | Logout</span>
     </a>
 </li>
 
